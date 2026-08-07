5a0f00ad65d30f96746855c213e6d08d0337903e
Lor <tee.johnlor@gmail.com>
2026-08-07 23:50:25 +0800

    feat: spacing fix

--- src/dot_product_asm.asm
+++ src/dot_product_asm.asm
@@ -3,7 +3,7 @@ global dot_product_asm
 dot_product_asm:
     subsd xmm0, xmm0            ; xmm0 (sum) = 0.0
 .loop:
-    movsd  xmm1, qword [rsi]    ; xmm1 = A[i]
+    movsd xmm1, qword [rsi]     ; xmm1 = A[i]
     movsd xmm2, qword [rdx]     ; xmm2 = B[i]
     mulsd xmm1, xmm2            ; xmm1 = A[i] * B[i]
     addsd xmm0, xmm1            ; sum += A[i] * B[i]
